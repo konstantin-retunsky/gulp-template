6e451e5085f9f34f67d2f7e6c2f9d634ed7864e5
Konstantin Retunsky <konstantin.retunsky@gmail.com>
2021-03-04 13:28:25 +0500

    add  gulp-w3c-html-validator

--- gulp/tasks/pugHtml.js
+++ gulp/tasks/pugHtml.js
@@ -2,14 +2,15 @@ const { src, dest, parallel, series, watch } = require('gulp')
 const pug = require('gulp-pug')
 const rename = require('gulp-rename')
 const notify = require("gulp-notify")
+const gulpHtmlBemValidator = require('gulp-html-bem-validator');
 
 const pathBuildPug = './source/pages/**/*.pug'
 
 module.exports = function pugHtml(cb) {
   return src(pathBuildPug)
-  // .pipe(plumber())
   .pipe(pug({pretty: false}))
     .on('error', notify.onError((err) => `Pug ERROR: ${err.message}`))
   .pipe(rename({dirname: ''}))
+  .pipe(gulpHtmlBemValidator())
   .pipe(dest('build'));
 }

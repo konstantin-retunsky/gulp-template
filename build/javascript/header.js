6a5757607d9f5fa4fde3197885a2e4fa167f643a
Konstantin Retunsky <konstantin.retunsky@gmail.com>
2021-03-04 22:28:08 +0500

    #

--- build/javascript/header.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-console.log('test');
-var array = [1, 2, 3, 4];
-array.map(function (number) {
-  return console.log(number);
-})[document.querySelectorAll('a')].forEach(function (element) {
-  console.log(element);
-});
